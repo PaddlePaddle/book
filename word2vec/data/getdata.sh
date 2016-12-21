c21bd0155dfe462f51d96035abca85270b330e63
zhangruiqing01 <zhangruiqing01@baidu.com>
2016-12-21 15:35:29 +0800

    add 2nd-period tutorial catelog

--- word2vec/data/getdata.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-tar -zxf simple-examples.tgz
-echo `pwd`/simple-examples/data/ptb.train.txt > train.list
-echo `pwd`/simple-examples/data/ptb.valid.txt > test.list
