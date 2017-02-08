1d99996617347f58a6bb26a6280103bc0e1b606d
liaogang <liaogang@baidu.com>
2017-02-08 19:04:24 +0800

    Update code

--- recommender_system/train.py
+++ recommender_system/train.py
@@ -1,8 +1,7 @@
-from paddle import layers
-from paddle import model
-from paddle import optimizer
-
-from paddle.datasets import movielens
+import paddle.layer
+import paddle.model
+import paddle.optimizer
+import paddle.datasets.movielens
 
 
 def main():
@@ -10,54 +9,55 @@ def main():
     paddle.init(use_gpu = True)
 
     # Keras
-    X_train, y_train, X_test, y_test = movielens.load_data('xxx')
+    (X_train, y_train), (X_test, y_test) = paddle.datasets.movielens.load_data('xxx')
 
     embsize = 256
 
     # It's better if we can refactor network, but I did not figure out now.
 
     # construct movie feature
-    mv_id = data_layer('mv_id', size = xxx))
-    title = data_layer('title', size = xxx))
-    geres = data_layer('geres', size = xxx))
+    mv_id = paddle.layer.data('mv_id', size = xxx)
+    title = paddle.layer.data('title', size = xxx)
+    geres = paddle.layer.data('geres', size = xxx)
 
-    movie_id_emb = embedding_layer(input=mv_id, size=embsize))
-    movie_id_hid = fc_layer(input=movie_id_emb, size=embsize))
-    genres_embed = fc_layer(input=geres, size=embsize))
-    titles_embed = embedding_layer(input=title, size=embsize)）
-    title_hidden = text_conv_pool(input=titles_embed, context_len=5, hidden_size=embsize))
-    movie_feature = fc_layer(input=[movie_id_hid, title_hidden, genres_embed], size=embsize))
+    movie_id_emb = paddle.layer.embedding(input=mv_id, size=embsize)
+    movie_id_hid = paddle.layer.fc(input=movie_id_emb, size=embsize)
+    genres_embed = paddle.layer.fc(input=geres, size=embsize)
+    titles_embed = paddle.layer.embedding(input=title, size=embsize)
+    title_hidden = paddle.layer.text_conv_pool(input=titles_embed, context_len=5, hidden_size=embsize))
+    movie_feture = paddle.layer.fc_layer(input=[movie_id_hid, title_hidden, genres_embed], size=embsize))
     
     # construct user feature
-    user_id = data_layer(size = xxx))
-    gender = data_layer(size = xxx))
-    age = data_layer(size = xxx))
-    occupation = data_layer(size = xxx))
+    user_id = paddle.layer.data(size = xxx))
+    gender = paddle.layer.data(size = xxx))
+    age  = paddle.layer.data(size = xxx))
+    occupation = paddle.layer.data(size = xxx))
 
-    user_id_emb = embedding_layer(input=user_id, size=embsize))
-    user_id_hidden = fc_layer(input=user_id_emb, size=embsize))
-    gender_emb = embedding_layer(input=gender, size=embsize))
-    gender_hidden = fc_layer(input=gender_emb, size=embsize))
-    age_emb = embedding_layer(input=age, size=embsize))
-    age_hidden = fc_layer(input=age_emb, size=embsize))
-    occup_emb = embedding_layer(input=occupation, size=embsize))
-    occup_hidden = fc_layer(input=occup_emb, size=embsize))
-    user_feature = fc_layer(
-        input=[user_id_hidden, gender_hidden, age_hidden, occup_hidden], size=embsize)
+    user_id_emb = paddle.layer.embedding(input=user_id, size=embsize)
+    user_id_hid = paddle.layer.fc(input=user_id_emb, size=embsize)
 
-    similarity = cos_sim(a=movie_feature, b=user_feature, scale=2))
-    lbl = data_layer('rating', size=1))
-    cost = regression_cost(input=similarity, label=lbl))
+    gender_embd = paddle.layer.embedding(input=gender, size=embsize)
+    gender_hidd = paddle.layer.fc(input=gender_embd, size=embsize)
 
-    # how to refactor and line up network to make it more readable and elegant
-    model.network(cost)
+    age_embding = paddle.layer.embedding(input=age, size=embsize)
+    age_hiddens = paddle.layer.fc(input=age_embding, size=embsize)
 
-    # optimization
-    model.optimizer(RMSprop(batch_size = 1600, learning_rate = 1e-3))
+    occup_embed = paddle.layer.embedding(input=occupation, size=embsize)
+    occup_hiden = paddle.layer.fc(input=occup_embed, size=embsize)
 
-    # can we make it more precise? keras only use a fit function in here
-    model.train(X_train, y_train, plot=True)
-    model.test(X_test, y_test, plot=True)
+    user_feture = paddle.layer.fc(
+        input=[user_id_hid, gender_hidd, age_hidden, occup_hiden], size=embsize)
+    
+    sim = paddle.layer.cos_sim(a=movie_feature, b=user_feature, scale=2)
+
+    model = paddle.model.create(sim)
+
+    lbl = paddle.layer.data('rating', size=1)
+    RMSprop.train(model, paddle.cost.regression(sim, lbl), 
+                  batch_size = 1600, learning_rate = 1e-3, ...)
+
+
+
 
 if __name__ == '__main__':
     main()
