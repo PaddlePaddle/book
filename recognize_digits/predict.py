e9ba37bc5d3c8fe91559668b81b02b2bdca8ca19
dayhaha <18800111918@163.com>
2017-02-08 09:08:50 +0000

    fix mnist predict.py bug

--- recognize_digits/predict.py
+++ recognize_digits/predict.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Usage: predict.py -c CONF -d ./data/raw_data/  -m MODEL
+"""Usage: predict.py -c CONF -d DATA  -m MODEL
 
 
 Arguments:
