231622d5c57c5f4a5448e2798fdc422261c8746d
gongweibao <gongweibao@baidu.com>
2017-03-08 15:28:03 +0800

    modify travis

--- pre-commit-hooks/convert_markdown_into_ipynb.sh
+++ pre-commit-hooks/convert_markdown_into_ipynb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 command -v go >/dev/null 2>&1
-if [[ $? != 0 ]]; then
+if [ $? -ne 0 ]; then
     echo >&2 "Please install https://golang.org/doc/install#install"
     exit 1
 fi
@@ -10,7 +10,7 @@ GOPATH=/tmp/go go get -u github.com/wangkuiyi/ipynb/markdown-to-ipynb
 
 for file in $@ ; do
 	/tmp/go/bin/markdown-to-ipynb < $file > ${file%.*}".ipynb"
-    if [[ $? != 0 ]]; then
+    if [ $? -ne 0 ]; then
         echo >&2 "markdown-to-ipynb $file error"
         exit 1
     fi
--- tools/convert-markdown-into-ipynb-and-test.sh
+++ tools/convert-markdown-into-ipynb-and-test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 command -v go >/dev/null 2>&1
-if [[ $? != 0 ]]; then
+if [ $? -ne 0 ]; then
     echo >&2 "Please install go https://golang.org/doc/install#install"
     exit 1
 fi
 
-GOPATH=/tmp/go go get -u github.com/wangkuiyi/ipynb/markdown-to-ipynb
+#GOPATH=/tmp/go go get -u github.com/wangkuiyi/ipynb/markdown-to-ipynb
 
 cur_path=$(dirname $(readlink -f $0))
 cd $cur_path/../
@@ -13,7 +13,7 @@ cd $cur_path/../
 #convert md to ipynb
 for file in */{README,README\.en}.md ; do
     /tmp/go/bin/markdown-to-ipynb < $file > ${file%.*}".ipynb"
-    if [[ $? != 0 ]]; then
+    if [ $? -ne 0 ]; then
         echo >&2 "markdown-to-ipynb $file error"
         exit 1
     fi
