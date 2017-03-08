2096235f46125705051f15df081205666fc74d39
gongweibao <gongweibao@baidu.com>
2017-03-08 19:48:41 +0800

    rm not need

--- pre-commit-hooks/convert_markdown_into_ipynb.sh
+++ pre-commit-hooks/convert_markdown_into_ipynb.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-command -v go >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo >&2 "Please install https://golang.org/doc/install#install"
-    exit 1
-fi
-
-#convert to ipynb file
-#GOPATH=/tmp/go go get -u github.com/wangkuiyi/ipynb/markdown-to-ipynb
-
 for file in $@ ; do
 	/tmp/go/bin/markdown-to-ipynb < $file > ${file%.*}".ipynb"
     if [ $? -ne 0 ]; then
