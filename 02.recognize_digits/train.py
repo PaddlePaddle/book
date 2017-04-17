be45facf66ee9e4ba7fdab22c25e00e83ca818d0
qijun <qijun1994@hotmail.com>
2017-04-17 13:58:23 +0800

    refine some codes

--- 02.recognize_digits/train.py
+++ 02.recognize_digits/train.py
@@ -44,57 +44,61 @@ def convolutional_neural_network(img):
         input=conv_pool_2, size=10, act=paddle.activation.Softmax())
     return predict
 
+def main():
+    paddle.init(use_gpu=False, trainer_count=1)
 
-paddle.init(use_gpu=False, trainer_count=1)
+    # define network topology
+    images = paddle.layer.data(
+        name='pixel', type=paddle.data_type.dense_vector(784))
+    label = paddle.layer.data(name='label', type=paddle.data_type.integer_value(10))
 
-# define network topology
-images = paddle.layer.data(
-    name='pixel', type=paddle.data_type.dense_vector(784))
-label = paddle.layer.data(name='label', type=paddle.data_type.integer_value(10))
+    # Here we can build the prediction network in different ways. Please
+    # choose one by uncomment corresponding line.
+    # predict = softmax_regression(images)
+    # predict = multilayer_perceptron(images)
+    predict = convolutional_neural_network(images)
 
-# Here we can build the prediction network in different ways. Please
-# choose one by uncomment corresponding line.
-# predict = softmax_regression(images)
-# predict = multilayer_perceptron(images)
-predict = convolutional_neural_network(images)
+    cost = paddle.layer.classification_cost(input=predict, label=label)
 
-cost = paddle.layer.classification_cost(input=predict, label=label)
+    parameters = paddle.parameters.create(cost)
 
-parameters = paddle.parameters.create(cost)
+    optimizer = paddle.optimizer.Momentum(
+        learning_rate=0.1 / 128.0,
+        momentum=0.9,
+        regularization=paddle.optimizer.L2Regularization(rate=0.0005 * 128))
 
-optimizer = paddle.optimizer.Momentum(
-    learning_rate=0.1 / 128.0,
-    momentum=0.9,
-    regularization=paddle.optimizer.L2Regularization(rate=0.0005 * 128))
+    trainer = paddle.trainer.SGD(
+        cost=cost, parameters=parameters, update_equation=optimizer)
 
-trainer = paddle.trainer.SGD(
-    cost=cost, parameters=parameters, update_equation=optimizer)
-
-lists = []
-
-
-def event_handler(event):
-    if isinstance(event, paddle.event.EndIteration):
-        if event.batch_id % 100 == 0:
-            print "Pass %d, Batch %d, Cost %f, %s" % (
-                event.pass_id, event.batch_id, event.cost, event.metrics)
-    if isinstance(event, paddle.event.EndPass):
-        result = trainer.test(reader=paddle.batch(
-            paddle.dataset.mnist.test(), batch_size=128))
-        print "Test with Pass %d, Cost %f, %s\n" % (event.pass_id, result.cost,
-                                                    result.metrics)
-        lists.append((event.pass_id, result.cost,
-                      result.metrics['classification_error_evaluator']))
+    lists = []
 
 
-trainer.train(
-    reader=paddle.batch(
-        paddle.reader.shuffle(paddle.dataset.mnist.train(), buf_size=8192),
-        batch_size=128),
-    event_handler=event_handler,
-    num_passes=100)
+    def event_handler(event):
+        if isinstance(event, paddle.event.EndIteration):
+            if event.batch_id % 100 == 0:
+                print "Pass %d, Batch %d, Cost %f, %s" % (
+                    event.pass_id, event.batch_id, event.cost, event.metrics)
+        if isinstance(event, paddle.event.EndPass):
+            result = trainer.test(reader=paddle.batch(
+                paddle.dataset.mnist.test(), batch_size=128))
+            print "Test with Pass %d, Cost %f, %s\n" % (event.pass_id, 
+                                                        result.cost,
+                                                        result.metrics)
+            lists.append((event.pass_id, result.cost,
+                            result.metrics['classification_error_evaluator']))
 
-# find the best pass
-best = sorted(lists, key=lambda list: float(list[1]))[0]
-print 'Best pass is %s, testing Avgcost is %s' % (best[0], best[1])
-print 'The classification accuracy is %.2f%%' % (100 - float(best[2]) * 100)
+
+    trainer.train(
+        reader=paddle.batch(
+            paddle.reader.shuffle(paddle.dataset.mnist.train(), buf_size=8192),
+            batch_size=128),
+        event_handler=event_handler,
+        num_passes=100)
+
+    # find the best pass
+    best = sorted(lists, key=lambda list: float(list[1]))[0]
+    print 'Best pass is %s, testing Avgcost is %s' % (best[0], best[1])
+    print 'The classification accuracy is %.2f%%' % (100 - float(best[2]) * 100)
+
+if __name__ == '__main__':
+    main()    
