578bc2f34babcf0bd9ddda8c9cbd140de7ea6e68
liaogang <liaogang@baidu.com>
2017-04-24 12:53:47 +0800

    add notedown to covert md to ipynb

---
+++ .tools/notedown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+pip install notedown
+
+cur_path="$(cd "$(dirname "$0")" && pwd -P)"
+cd $cur_path/../
+
+#convert md to ipynb
+for file in */{README,README\.en}.md ; do
+    notedown $file > ${file%.*}.ipynb
+done
