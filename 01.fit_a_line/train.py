9ba33ad06e5e46f0c3a1da2a982ddccfc89ed004
typhoonzero <typhoonzero1986@gmail.com>
2017-09-08 10:08:10 +0800

    fix rename mse_cost

--- 01.fit_a_line/train.py
+++ 01.fit_a_line/train.py
@@ -10,7 +10,7 @@ def main():
     x = paddle.layer.data(name='x', type=paddle.data_type.dense_vector(13))
     y_predict = paddle.layer.fc(input=x, size=1, act=paddle.activation.Linear())
     y = paddle.layer.data(name='y', type=paddle.data_type.dense_vector(1))
-    cost = paddle.layer.mse_cost(input=y_predict, label=y)
+    cost = paddle.layer.square_error_cost(input=y_predict, label=y)
 
     # create parameters
     parameters = paddle.parameters.create(cost)
