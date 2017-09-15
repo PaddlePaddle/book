1c3d34ebdeb1e2b2879b30b520aeaa7391603337
ludaming <ludaming@baidu.com>
2017-09-14 21:39:39 -0700

    done

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,43 +14,43 @@ then
 fi
 
 mkdir $directory_name
+
 cp -r .tools/ $directory_name/.tools
-#cp ../01.fit_a_line/ $directory_name
-for i in `ls -F | grep /`
-do
-echo $i
-should_copy=false
-cd $i
-if [ -e index.html ] && [ -e index.cn.html ] && [ -d image ]
-then
-    echo "ok"
-    should_copy=true
-else
-    echo "nok"
-fi
-cd ..
 
-if $should_copy ; then
-  echo 'should_copy'
-  mkdir $directory_name/$i
-  cp $i/index.html $directory_name/$i
-  cp $i/index.cn.html $directory_name/$i
-  cp -r $i/image $directory_name/$i
-fi
+for i in `ls -F | grep /` ; do
+    should_copy=false
+    cd $i
 
-cp index.html $directory_name/
-cp index.cn.html $directory_name/
+    if [ -e index.html ] && [ -e index.cn.html ] && [ -d image ]
+    then
+        should_copy=true
+    fi
+
+    cd ..
+
+    if $should_copy ; then
+      mkdir $directory_name/$i
+      cp $i/index.html $directory_name/$i
+      cp $i/index.cn.html $directory_name/$i
+      cp -r $i/image $directory_name/$i
+    fi
+
+    cp index.html $directory_name/
+    cp index.cn.html $directory_name/
 
 done
 
-openssl enc -in ubuntu.pem.enc -out ubuntu.pem -d -aes256 -k $DEC_PASSWD
-##openssl enc -in ubuntu.pem.enc -out ubuntu.pem -d -aes256 -k 'SOME_PASSWD'
+openssl aes-256-cbc -d -a -in ubuntu.pem.enc -out ubuntu.pem -k $DEC_PASSWD
+
 eval "$(ssh-agent -s)"
 chmod 400 ubuntu.pem
+
 ssh-add ubuntu.pem
-rsync -r --delete-after --quiet ../build ubuntu@52.76.173.135:/tmp/
-#rsync -r --delete-after --quiet ../01.fit_a_line ubuntu@52.76.173.135
+rsync -r build/ ubuntu@52.76.173.135:/tmp/book
 
+rm -rf $directory_name
 
-#ssh ubuntu@52.76.173.135 '/usr/bin/touch /tmp/ok'
+chmod 644 ubuntu.pem
+rm ubuntu.pem
+
 trap : 0
