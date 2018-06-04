6c30c1edbce4a9df70ecf4179ecaff8219dacffb
Wang,Jeff <wangjeff@baidu.com>
2018-06-04 16:55:00 -0700

    Update the train.py as the base.

--- 03.image_classification/vgg.py
+++ 03.image_classification/vgg.py
@@ -12,36 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import paddle.v2 as paddle
+import paddle
+import paddle.fluid as fluid
 
 __all__ = ['vgg_bn_drop']
 
 
 def vgg_bn_drop(input):
-    def conv_block(ipt, num_filter, groups, dropouts, num_channels=None):
-        return paddle.networks.img_conv_group(
+    def conv_block(ipt, num_filter, groups, dropouts):
+        return fluid.nets.img_conv_group(
             input=ipt,
-            num_channels=num_channels,
             pool_size=2,
             pool_stride=2,
             conv_num_filter=[num_filter] * groups,
             conv_filter_size=3,
-            conv_act=paddle.activation.Relu(),
+            conv_act='relu',
             conv_with_batchnorm=True,
             conv_batchnorm_drop_rate=dropouts,
-            pool_type=paddle.pooling.Max())
+            pool_type='max')
 
-    conv1 = conv_block(input, 64, 2, [0.3, 0], 3)
+    conv1 = conv_block(input, 64, 2, [0.3, 0])
     conv2 = conv_block(conv1, 128, 2, [0.4, 0])
     conv3 = conv_block(conv2, 256, 3, [0.4, 0.4, 0])
     conv4 = conv_block(conv3, 512, 3, [0.4, 0.4, 0])
     conv5 = conv_block(conv4, 512, 3, [0.4, 0.4, 0])
 
-    drop = paddle.layer.dropout(input=conv5, dropout_rate=0.5)
-    fc1 = paddle.layer.fc(input=drop, size=512, act=paddle.activation.Linear())
-    bn = paddle.layer.batch_norm(
-        input=fc1,
-        act=paddle.activation.Relu(),
-        layer_attr=paddle.attr.Extra(drop_rate=0.5))
-    fc2 = paddle.layer.fc(input=bn, size=512, act=paddle.activation.Linear())
-    return fc2
+    drop = fluid.layers.dropout(x=conv5, dropout_prob=0.5)
+    fc1 = fluid.layers.fc(input=drop, size=512, act=None)
+    bn = fluid.layers.batch_norm(input=fc1, act='relu')
+    drop2 = fluid.layers.dropout(x=bn, dropout_prob=0.5)
+    fc2 = fluid.layers.fc(input=drop2, size=512, act=None)
+    predict = fluid.layers.fc(input=fc2, size=10, act='softmax')
+    return predict
